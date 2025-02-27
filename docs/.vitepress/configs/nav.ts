a1c321fe0863248126ddf0984e3a299dd6683d89
Douglas <652161438@qq.com>
2025-02-27 11:19:24 +0800

    01-项目起航

---
+++ docs/.vitepress/configs/nav.ts
@@ -0,0 +1,15 @@
+import type { DefaultTheme } from 'vitepress'
+
+export const nav: DefaultTheme.Config['nav'] = [
+  { text: '提效工具', link: '/nav/' },
+  { text: '我的小册', link: '' },
+  {
+    text: '生活记录',
+    link: '',
+  },
+  { text: '个人项目', link: '' },
+  {
+    text: '关于我',
+    link: '',
+  },
+]
