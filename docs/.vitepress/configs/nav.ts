3ba8c7bbffc061e6ecf3c7d8746377f16e44b3c0
apple <652161438@qq.com>
2025-03-08 10:43:54 +0800

    02-修复bug

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -2,7 +2,24 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
   { text: '提效工具', link: '/nav/' },
-  { text: '我的小册', link: '' },
+  {
+    text: '我的小册',
+    // link: '/booklet/',
+    items: [
+      {
+        text: 'Vue3 全家桶',
+        link: '/booklet/vue3/',
+      },
+      {
+        text: 'Vitepress',
+        link: '/booklet/vitepress/',
+      },
+      {
+        text: 'python',
+        link: '/booklet/python/',
+      },
+    ],
+  },
   {
     text: '生活记录',
     link: '',
@@ -10,6 +27,10 @@ export const nav: DefaultTheme.Config['nav'] = [
   { text: '个人项目', link: '' },
   {
     text: '关于我',
-    link: '',
+    link: '/about',
+  },
+  {
+    text: 'todo',
+    link: '/todo',
   },
 ]
